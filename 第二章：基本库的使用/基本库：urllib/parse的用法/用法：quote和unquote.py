60adabacd75382766527121ef8d0bbe54fc090e8
xiaowangAndyismengxin <177188505@qq.com>
2025-01-19 19:13:35 +0800

    Completed parse, urllib

---
+++ 第二章：基本库的使用/基本库：urllib/parse的用法/用法：quote和unquote.py
@@ -0,0 +1,12 @@
+from urllib.parse import quote, unquote  # 编码与解码
+from urllib.parse import urlparse, parse_qs
+
+
+question = "壁纸"
+url = "https://www.baidu.com/s?wd=" + quote(question)
+print(url)
+
+# 复现
+query = urlparse(url).query
+q = unquote(parse_qs(query)['wd'][0])
+print(q)
