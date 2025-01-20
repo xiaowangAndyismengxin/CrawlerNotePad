dd7a55e6fccc4ecd36549ef3a0e6da3bb250b7c9
xiaowangAndyismengxin <177188505@qq.com>
2025-01-21 00:29:23 +0800

    completed cookie, request

---
+++ 第二章：基本库的使用/基本库：requests/添加cookie/利用Session维持数据.py
@@ -0,0 +1,9 @@
+import requests
+
+
+s = requests.Session()
+s.get('https://www.httpbin.org/cookies/set/number/123456789')
+print(s.cookies.get_dict())
+r = s.get('https://www.httpbin.org/cookies')
+print(r.text)
+
---
+++ 第二章：基本库的使用/基本库：requests/添加cookie/获取cookie.py
@@ -0,0 +1,19 @@
+import requests
+
+
+url = "https://www.baidu.com"
+headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 "
+                  "Safari/537.36",
+}
+r = requests.get(url, headers=headers)
+print(type(r.cookies), r.cookies)
+print(r.cookies.get('BAIDUID'))
+print(r.cookies.set('a', 'b'))
+print(r.cookies.get_dict())
+print(r.cookies.keys())
+print(r.cookies.values())
+for k, v in r.cookies.items():
+    print(k, '=', v)
+r.cookies.clear()
+print(r.cookies.get_dict())
