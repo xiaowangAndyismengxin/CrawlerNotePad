7f3792fbbefff584865ffc8dce1b27b53f3ef330
xiaowangAndyismengxin <177188505@qq.com>
2025-02-01 22:55:22 +0800

    cancel verity setting in requests, password

--- 第二章：基本库的使用/基本库：requests/密码处理.py
+++ 第二章：基本库的使用/基本库：requests/密码处理.py
@@ -2,8 +2,7 @@ import requests
 from requests.auth import HTTPBasicAuth
 
 
-VERITY = False
-r = requests.get('https://ssr3.scrape.center', auth=('admin', 'admin'), verify=VERITY)
+r = requests.get('https://ssr3.scrape.center', auth=('admin', 'admin'))
 # =
 # r = requests.get('https://ssr3.scrape.center', auth=HTTPBasicAuth('admin', 'admin'), verity=VERITY)
 print(r.text)
