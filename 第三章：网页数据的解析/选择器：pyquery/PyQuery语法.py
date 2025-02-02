9ba0aef43445fe1d71522b0341b1a7317d637970
xiaowangAndyismengxin <177188505@qq.com>
2025-02-02 16:21:56 +0800

    PyQuery check ok

--- 第三章：网页数据的解析/选择器：pyquery/PyQuery语法.py
+++ 第三章：网页数据的解析/选择器：pyquery/PyQuery语法.py
@@ -6,7 +6,7 @@ doc = PyQuery(filename='../example_web.html')
 
 # 查找节点
 # 子孙节点
-# result = doc('.toppings').find('li')
+result = doc('.toppings').find('li')
 # 子节点
 # result = doc('.toppings').children()
 # 父节点
@@ -23,25 +23,25 @@ doc = PyQuery(filename='../example_web.html')
 #     print(li, type(li))
 
 # 属性获取
-result = doc('a').attr('href')
-result = doc('body a:only-child').attr.href
-result = doc('ul li:nth-of-type(2)').text()  # str
-result = repr(doc('ul li').text())  # str
+# result = doc('a').attr('href')
+# result = doc('body a:only-child').attr.href
+# result = doc('ul li:nth-of-type(2)').text()  # str
+# result = repr(doc('ul li').text())  # str
 
 # ---------------------------------------------------------------
 
 # 节点操作
-element_a = doc('a.title_a')
-result = element_a
+# element_a = doc('a.title_a')
+# result = element_a
 # element_a.remove_class('first_a')
 # element_a.add_class('first_a')
 
-element_a.attr('name', 'aaaa')
-element_a.remove_attr('name')
-element_a.text('new text')
-element_a.html('<sth>change html directly</sth>')
-element_sth = element_a.find('sth')
-print(element_sth.remove())
+# element_a.attr('name', 'aaaa')
+# element_a.remove_attr('name')
+# element_a.text('new text')
+# element_a.html('<sth>change html directly</sth>')
+# element_sth = element_a.find('sth')
+# print(element_sth.remove())
 
 
 print('result: ', result)
