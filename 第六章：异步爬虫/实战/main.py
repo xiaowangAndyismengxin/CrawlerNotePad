eb3285fc76ec9fe3760bb0bf5c456677eaf382aa
xiaowangAndyismengxin <177188505@qq.com>
2025-03-06 21:54:39 +0800

    Fix bug in crawler c6.

--- 第六章：异步爬虫/实战/main.py
+++ 第六章：异步爬虫/实战/main.py
@@ -2,5 +2,5 @@ from crawler import AsyncScrapeSPA5
 import asyncio
 
 
-crawler = AsyncScrapeSPA5(10, 3, default_delay=15)
+crawler = AsyncScrapeSPA5(504, 3, default_delay=15)
 asyncio.run(crawler.save_data())
